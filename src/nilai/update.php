40f344ed9bd7102ae90b5f400c9b48248f7f9926
noreply <noreply@please.com>
2021-12-22 18:36:08 +0700

    update thing

---
+++ src/nilai/update.php
@@ -0,0 +1,31 @@
+<?
+	require_once __DIR__ . '/../connect.inc';
+
+	$idNilai = $_POST['idn'];
+	$idMatkul = $_POST['idmatkul'];
+	$tugas = $_POST['tugas'];
+	$nilai = $_POST['nilai'];
+
+	$q = oci_parse($conn, "UPDATE nilai SET tugas=:tugas, nilai=:nilai, 
+	id_matakuliah=:idm WHERE id_nilai=:idn");
+	if (!$q) {
+		$e = oci_error($conn);
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	oci_bind_by_name($q, ":tugas", $tugas);
+	oci_bind_by_name($q, ":nilai", $nilai);
+	oci_bind_by_name($q, ":idm", $idMatkul);
+	oci_bind_by_name($q, ":idn", $idNilai);
+
+	$r = oci_execute($q);
+	if (!$r) {
+		$e = oci_error($q);
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	oci_free_statement($q);
+	oci_close($conn);
+
+	header("Location: http://localhost:8080/matakuliah/view.php?idmatkul={$idMatkul}", true, 303);
+	exit();
