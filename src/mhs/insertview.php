7d0c657134c46c9894d5fff22f6ba8ebc6acdb61
noreply <noreply@please.com>
2021-12-22 18:59:04 +0700

    back to mahasiswa

--- src/mhs/insertview.php
+++ src/mhs/insertview.php
@@ -1,3 +1,7 @@
+<?php 
+	require_once __DIR__ . "/../constant.inc";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,6 +28,12 @@
 <body>
 	<div class="centering">
 		<form action="insert.php" method="POST" class="container">
+			<a href="<?= "{$env['server']}/mhs/view.php"?>" class="btn btn-primary">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+				</svg>
+				Back to Mahasiswa
+			</a>
 			<h2 class="mb-3">Insert Mahasiswa</h2>
 			<div class="row mb-3">
 				<label for="nama" class="col form-label me-3">Nama</label>
